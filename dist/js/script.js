684bd75ac6c0cdbc0ed6ee0e3d93a6eda815d28c
Yevhen <tishenko13018@gmail.com>
2022-08-29 13:44:27 +0300

    menu-hamburger

---
+++ dist/js/script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    close = document.querySelector('.menu__close');
+//Получаем по селектору все эти элементы
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+}); //При нажатии на гамбургер появляется класс active
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+}); //При нажатии на крестик пропадает класс active
